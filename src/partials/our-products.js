e59c7f94c4cbb02be675894bcb97fd366da06d00
AnnaHort <annahort3d@gmail.com>
2023-04-09 02:34:06 +0300

    Finished swiper and full addaptive
    Залишаю коментарі до повного прийняття секції (видалю, як тільки все пройде перевірку і буде готове до злиття)

--- src/partials/our-products.js
+++ src/partials/our-products.js
@@ -1,41 +1,91 @@
-const el = document.querySelector('.my-element');
 
-const swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
-  effect: 'fade',
-  loop: true,
-  mousewheel: {
-    invert: false,
-  },
-  pagination: {
+new Swiper('.swiper', {
+// навігація
+  // булети, теперішнє положення, прогресбар
+  
+ pagination: {
     el: '.swiper-pagination',
     clickable: true,
+    dynamicBullets: false
   },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+
+  // включення/відключення перемикання слайду захватом картинки на пк
+  simulateTouch: true,
+
+  // чутливість свайпу
+  touchRatio: 1,
+
+  // кут зпрацьовування свайпу/перетягування
+  touchAngle: 45,
+
+  // курсор перетягування
+  grabCursor: true,
+
+  // перемикання при кліку на слайд
+slideToClickedSlide: true,
+  // навігація по хешу
+  hashNavigation: {
+  // відслідковувати стан
+    watchState: true,
   },
-  scrollbar: {
-    el: '.swiper-scrollbar',
+  // управління клавіатурою
+  keyboard: {
+    // увімкнути/вимкнути
+    enabled: true,
+    // вкл/викл тільки коли слайдер в межах viewport
+    pageUpDown: true,
   },
+
+  // управління колесом мишки
+  mousewheel: {
+// чутливість колеса миші
+    sensitivity: 1,
+    // клас об'єкта на якому буде спрацьовувати прокрутка мишкою
+    // (якщо на сторінці декілька слайдерів то вони будуть крутитись всі, тому варто залишити це за замовчуванням)
+    // eventsTarget: ".swiper"
+  },
+  // кількість слайдів для показу (по замовчуванню стоїть 'auto')
+  slidesPerView: 'auto', 
+   // відключення функціоналу, якщо слайдів менше ніж потрібно
+  watchOverflow: true,
+
+  // відступ між слайдами 
+  spaceBetween: 30,
+  // безкінечний слайдер
+  loop: true,
+   // відключаємо скрол + мультирядковість,якщо slidesPerView: 'auto'
+  // то потрібно прописувати к-сть дублюючих слайдів
+  loopedSlides: 3,
+  // вільний режим прокрутки
+  freeMode: true,
+
+  // автопрокрутка
+  autoplay: {
+// // пауза між прокруткою
+    delay: 3000,
+//     // закінчити на останньому слайді 
+    stopOnLastSlide: true,
+//     // відключити після ручного переключення
+    disableOnInteraction: false
+  },
+
+  // швидікість автопрокруту 
+  speed: 800,
+
+  // БРЕКПОІНТ / АДАПТИВ / 
+  // ширина екрану
+  // (виставити значення slidesPerView: 'auto')
   breakpoints: {
+    375: {
+      
+      slidesPreView: 1,
+    },
     768: {
       slidesPerView: 2.5,
-      spaceBetween: 30,
+      spaceBetween: 18,
     },
-    1024: {
-      slidesPerView: 3.5,
-      spaceBetween: 30,
+    1200: {
+      slidesPerView: 4,
     }
   },
-  slidesPerView: 2.5,
-  spaceBetween: 15,
-});
-
-if (el) {
-  const scrollbar = swiper.scrollbar;
-  const isHorizontal = swiper.isHorizontal();
-
-  scrollbar.el.classList.add(isHorizontal ? 'swiper-scrollbar-horizontal' : 'swiper-scrollbar-vertical');
-  scrollbar.dragEl.classList.add(isHorizontal ? 'swiper-scrollbar-drag-horizontal' : 'swiper-scrollbar-drag-vertical');
-} 
+})
