b68efba1674489eaaf4718ace3d1f6d2b1cde6ca
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-04-10 17:03:27 -0500

    update js

--- src/js/modal.explore.js
+++ src/js/modal.explore.js
@@ -1,29 +1,14 @@
-// Отримуємо кнопку підписки та модальне вікно
-var subscribeBtn = document.getElementById("subscribe-btn");
-var modal = document.getElementById("modal");
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-explore-open]"),
+    closeModalBtn: document.querySelector("[data-explore-close]"),
+    modal: document.querySelector("[data-backdrop]"),
+  };
 
-// Отримуємо кнопки закриття модального вікна та кнопку submit форми
-var closeBtn = document.getElementById("close-btn");
-var submitBtn = document.getElementById("submit-btn");
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-// Коли користувач натискає кнопку підписки, відкриваємо модальне вікно
-subscribeBtn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// Коли користувач натискає кнопку закриття, закриваємо модальне вікно
-closeBtn.onclick = function() {
-  modal.style.display = "none";
-}
-
-// Коли користувач успішно підписується, закриваємо модальне вікно
-submitBtn.onclick = function() {
-  modal.style.display = "none";
-}
-
-// Коли користувач натискає поза модальним вікном, закриваємо його
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
   }
-}
+})();
