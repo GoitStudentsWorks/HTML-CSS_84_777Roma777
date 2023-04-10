6be8adc32f520f0bccfbe2e679ffe2b24dbfaef8
777Roma777 <nanoeagle911@gmail.com>
2023-04-10 21:49:15 +0300

    add hero

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import './js/our-products'; /* 2 */
 import './js/how-its-made';
 import './js/hero';
 import './js/swiper';
-import ('./js/explore.js')
+import './js/explore.js';
 import './js/modal';
