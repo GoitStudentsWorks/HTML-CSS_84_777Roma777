6788300f077b7a7989fd5e2982fdd08933835bdd
777Roma777 <nanoeagle911@gmail.com>
2023-04-11 00:21:20 +0300

    change js and datamodal = number

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import './js/by-now-modal'; /* 1 */
-import './js/our-products'; /* 2 */
+import './js/by-now-modal';
+import './js/our-products';
 import './js/how-its-made';
-import './js/hero';
 import './js/swiper';
 import './js/explore.js';
 import './js/modal';
