eaa77356dfe0284dc7d6ae74d483acaacbff60b4
777Roma777 <nanoeagle911@gmail.com>
2023-04-11 13:29:51 +0300

    comment hero section

--- src/main.js
+++ src/main.js
@@ -4,10 +4,5 @@ import './js/modal-explore';
 import './js/how-its-made';
 import './js/swiper';
 import './js/modal';
-import './js/by-now-modal';
-import './js/our-products';
-import './js/how-its-made';
 import './js/swiper';
-import './js/explore.js';
-import './js/modal';
 
