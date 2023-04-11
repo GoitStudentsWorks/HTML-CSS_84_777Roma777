52a04ad4b9139224c23587806dfd5345afb6298b
777Roma777 <nanoeagle911@gmail.com>
2023-04-11 15:52:06 +0300

    review delete

--- src/main.js
+++ src/main.js
@@ -2,6 +2,4 @@ import './js/by-now-modal'; /* 1 */
 import './js/our-products'; /* 2 */
 import './js/modal-explore';
 import './js/how-its-made';
-import './js/swiper';
 import './js/modal';
-import './js/swiper';
