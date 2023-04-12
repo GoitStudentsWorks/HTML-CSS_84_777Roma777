5b9c03183c0fc145d28f646df30896cd9a09260d
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-04-12 08:31:00 -0500

    update js import

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './js/by-now-modal'; /* 1 */
 import './js/our-products'; /* 2 */
 import './js/modal-explore';
+import './js/modal-review';
 import './js/how-its-made';
 import './js/modal';
